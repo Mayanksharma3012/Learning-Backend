bf78474de09ce0d531733cdfbdb02f5f02b94035
Mayank Sharma <mayanks3012@gmail.com>
2026-07-13 11:49:16 +0530

    added Access and Refresh token in user model.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -47,7 +47,36 @@ userSchema.pre('save', async function(next){
     }
 })
 
+
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
+
+
+
+userSchema.methods.generateAccessToken = async function(){  
+    return await jwt.sign(
+        {    //payload
+            _id = this._id,
+            email = this.email,
+            userName = this.userName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = async function(){
+    return await jwt.sign(
+        {    //payload
+            _id = this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 export const User = mongoose.model('User', userSchema)
